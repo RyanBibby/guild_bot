6b8c4078087bb5a29d34e1518b5aef3194025495
Ryan Bibby <r.bibby@gmail.com>
2022-03-13 14:23:02 +0000

    Mess about with filepaths

--- lib/util/db.rb
+++ lib/util/db.rb
@@ -3,17 +3,12 @@ require 'json'
 class Db < OpenStruct
 
   def self.load
-    JSON.parse(File.read(file_location), object_class: self)
+    JSON.parse(File.read("#{$settings.base_path}/data/save.json"), object_class: self)
   end
 
   def save(field, value)
     self[field] = value
-    File.open(self.file_location, "w") { |f| f.write self.to_h.to_json }
+    File.open("#{$settings.base_path}/data/save.json", "w") { |f| f.write self.to_h.to_json }
     $db = self
   end
-
-  def self.file_location
-    "#{$settings.base_path}/data/save.json"
-  end
-
 end
